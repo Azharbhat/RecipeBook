78ec560e59b8c5bda38e3b20226842487b25bf7f
Azhar Mohammad Bhat <106708211+Azharbhat@users.noreply.github.com>
2024-02-29 12:04:47 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,34 +2,34 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import blogRouter from "./routes/blog-routes.js";
-import router from "./routes/user-routes.js";
+import userRouter from "./routes/user-routes.js";
 import cors from "cors";
-import bodyParser from 'body-parser';
 
 dotenv.config();
+
 const app = express();
+
+// Middleware
 app.use(cors({
-  origin:["https://recipe-book-frontend-nu.vercel.app"],
-  methods:["POST","GET"],
-  credentials:true
+  origin: ["https://recipe-book-frontend-nu.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json({ limit: '100mb' }));
-app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
-app.use("/api/user", router);
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
+
+// Routes
+app.use("/api/user", userRouter);
 app.use("/api/blog", blogRouter);
 
-const PORT = process.env.PORT || 8000;
-
-mongoose
-  .connect("mongodb+srv://azhar:Mongodb%406715@ambcluster.prfsnii.mongodb.net/RecipeBook", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://azhar:Mongodb%406715@ambcluster.prfsnii.mongodb.net/RecipeBook", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
+    console.log("Connected to MongoDB");
+    // Start the server after successful MongoDB connection
+    const PORT = process.env.PORT || 8000;
     app.listen(PORT, () => {
-      console.log(`Connected To Database and listening at PORT ${PORT}`);
+      console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
